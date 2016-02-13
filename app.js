bc8562c5bf7f117978b069d055e13232d83b5888
BlackGanglion <BlackGanglion@blackganglion.com>
2016-02-13 17:41:10 +0800

    add gzip
    add gzip

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var multer  = require('multer');
 
 var app = express();
 
+app.use(express.compress());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'ejs');
